bd2e012e59d3167c717663eb4affdc5300cc6f22
Qian Cheng <csnowstack@gmail.com>
2016-06-25 16:27:37 +0800

    添加动画状态的回调

--- app/src/main/java/com/cq/csearchview/CSearchView.java
+++ app/src/main/java/com/cq/csearchview/CSearchView.java
@@ -7,10 +7,13 @@ import android.animation.ObjectAnimator;
 import android.content.Context;
 import android.content.res.TypedArray;
 import android.graphics.Color;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.util.AttributeSet;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.animation.AccelerateDecelerateInterpolator;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.ImageView;
 import android.widget.RelativeLayout;
 
@@ -22,11 +25,14 @@ public class CSearchView extends RelativeLayout {
     private CEditText edtSearch;
     private boolean isShow = false;
     private String hint = "请输入相关搜索字符";
-    private long duration = 1000;
+    private long duration = 1000,alphaDuration=800;
     private AnimatorSet showAnimatorSet, hideAnimatorSet;
     private int searchDrawable, cancelDrawable;
     private int hintColor, textColor, lineColor, bgColor;
-
+    private boolean openWtihShowSoftInput;
+    private InputMethodManager imm;
+    private OnSearchViewManagerListener mOnSearchViewManagerListener;
+    private OnStatusChangeListener mOnStatusChangeListener;
     public CSearchView(Context context) {
         this(context, null);
     }
@@ -46,45 +52,56 @@ public class CSearchView extends RelativeLayout {
             textColor = typedArray.getColor(R.styleable.CSearchView_csv_textColor, Color.parseColor("#1D1D1D"));
             lineColor = typedArray.getColor(R.styleable.CSearchView_csv_lineColor, Color.parseColor("#AEAEAE"));
             bgColor = typedArray.getColor(R.styleable.CSearchView_csv_backgroundColor, Color.parseColor("#00000000"));
-
+            openWtihShowSoftInput=typedArray.getBoolean(R.styleable.CSearchView_csv_openShowSoftInput,true);
             searchDrawable = typedArray.getResourceId(R.styleable.CSearchView_csv_searchDrawable, R.mipmap.ic_search_36pt_3x);
             cancelDrawable = typedArray.getResourceId(R.styleable.CSearchView_csv_cancelDrawable, R.mipmap.ic_close_36pt_3x);
         } finally {
             typedArray.recycle();
         }
 
-
+        imm=(InputMethodManager)context.getSystemService(Context.INPUT_METHOD_SERVICE);
         ivSearch.setImageResource(searchDrawable);
         edtSearch.setLineColor(lineColor);
         edtSearch.setTextColor(textColor);
         edtSearch.setHintTextColor(hintColor);
         setBackgroundColor(bgColor);
 
+        initEvent();
+    }
+
+    private void initEvent() {
         ivCancel.setOnClickListener(new OnClickListener() {
             @Override
             public void onClick(final View v) {
                 if (isShow) {
                     if (hideAnimatorSet == null) {
-                        ObjectAnimator ivObjectAnimator = ObjectAnimator.ofFloat(ivSearch, "alpha", 0.8f, 0).setDuration(500);
-                        ObjectAnimator edtObjectAnimator = ObjectAnimator.ofFloat(edtSearch, "alpha", 0.8f, 0).setDuration(500);
+                        ObjectAnimator ivObjectAnimator = ObjectAnimator.ofFloat(ivSearch, "alpha", 0.9f, 0).setDuration(alphaDuration);
+                        ObjectAnimator edtObjectAnimator = ObjectAnimator.ofFloat(edtSearch, "alpha", 0.9f, 0).setDuration(alphaDuration);
+                        ObjectAnimator hideIconAnimator=ObjectAnimator.ofFloat(ivCancel,"alpha",0.9f,0).setDuration(alphaDuration/2);
+                        final ObjectAnimator showAnimator= ObjectAnimator.ofFloat(ivCancel,"alpha",0,1).setDuration(alphaDuration/2);
+                        hideIconAnimator.addListener(new AnimatorListenerAdapter() {
+                            @Override
+                            public void onAnimationEnd(Animator animation) {
+                                super.onAnimationEnd(animation);
+                                ivCancel.setImageResource(searchDrawable);
+                                showAnimator.start();
+                            }
+                        });
+
 
                         hideAnimatorSet = new AnimatorSet();
-                        hideAnimatorSet.playTogether(ivObjectAnimator, edtObjectAnimator);
+                        hideAnimatorSet.playTogether(ivObjectAnimator, edtObjectAnimator,hideIconAnimator);
                         hideAnimatorSet.addListener(new AnimatorListenerAdapter() {
                             @Override
                             public void onAnimationEnd(Animator animation) {
                                 super.onAnimationEnd(animation);
-                                edtSearch.setVisibility(INVISIBLE);
-                                ivSearch.setVisibility(INVISIBLE);
-                                edtSearch.setAlpha(1f);
-                                ivSearch.setAlpha(1f);
-                                edtSearch.setText("");
+                                setHideSearchEnd();
                             }
 
                             @Override
                             public void onAnimationStart(Animator animation) {
                                 super.onAnimationStart(animation);
-                                ivCancel.setImageResource(searchDrawable);
+                                setHideSearchStar();
                             }
                         });
                     }
@@ -96,28 +113,24 @@ public class CSearchView extends RelativeLayout {
                 } else {
                     if (showAnimatorSet == null) {
 
-                        ObjectAnimator ivObjectAnimator = ObjectAnimator.ofFloat(ivSearch, "translationX", ivCancel.getLeft(), 0).setDuration(duration);
-                        ObjectAnimator edtObjectAnimator = ObjectAnimator.ofFloat(edtSearch, "translationX", ivCancel.getLeft() - (edtSearch.getLeft() - ivSearch.getRight()), 0).setDuration(duration);
+                        ObjectAnimator ivObjectAnimator = ObjectAnimator.ofFloat(ivSearch, "translationX", ivCancel.getLeft()-ivSearch.getLeft(), 0).setDuration(duration);
+                        ObjectAnimator edtObjectAnimator = ObjectAnimator.ofFloat(edtSearch, "translationX", ivCancel.getRight()-edtSearch.getLeft(), 0).setDuration(duration);
+                        ObjectAnimator showAnimator= ObjectAnimator.ofFloat(ivCancel,"alpha",0,1).setDuration(alphaDuration);
+
                         showAnimatorSet = new AnimatorSet();
                         showAnimatorSet.setInterpolator(new AccelerateDecelerateInterpolator());
-                        showAnimatorSet.playTogether(ivObjectAnimator, edtObjectAnimator);
+                        showAnimatorSet.playTogether(ivObjectAnimator, edtObjectAnimator,showAnimator);
                         showAnimatorSet.addListener(new AnimatorListenerAdapter() {
                             @Override
                             public void onAnimationStart(Animator animation) {
                                 super.onAnimationStart(animation);
-                                ivSearch.setVisibility(VISIBLE);
-                                edtSearch.setVisibility(VISIBLE);
-                                edtSearch.setCursorVisible(false);
-                                edtSearch.setHint("");
-                                ivCancel.setImageResource(cancelDrawable);
+                                setShowSearchStart();
                             }
 
                             @Override
                             public void onAnimationEnd(Animator animation) {
                                 super.onAnimationEnd(animation);
-                                edtSearch.setCursorVisible(true);
-                                edtSearch.requestFocus();
-                                edtSearch.setHint(hint);
+                                setShowSearchEnd();
                             }
                         });
                     }
@@ -132,6 +145,72 @@ public class CSearchView extends RelativeLayout {
                 isShow = !isShow;
             }
         });
+
+        edtSearch.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                if(null!=mOnSearchViewManagerListener){
+                    mOnSearchViewManagerListener.onTextChangeListener(s);
+                }
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
+
+    }
+
+    private void setHideSearchStar() {
+        imm.hideSoftInputFromWindow(edtSearch.getWindowToken(),0);
+        edtSearch.setCursorVisible(false);
+        if(null!=mOnStatusChangeListener){
+            mOnStatusChangeListener.onHideStartListener();
+        }
+
+    }
+
+
+
+
+    private void setHideSearchEnd() {
+        edtSearch.setVisibility(INVISIBLE);
+        ivSearch.setVisibility(INVISIBLE);
+        edtSearch.setAlpha(1f);
+        ivSearch.setAlpha(1f);
+        edtSearch.setText("");
+        if(null!=mOnStatusChangeListener){
+            mOnStatusChangeListener.onHideEndListener();
+        }
+    }
+
+    private void setShowSearchStart() {
+        ivSearch.setVisibility(VISIBLE);
+        edtSearch.setVisibility(VISIBLE);
+        edtSearch.setCursorVisible(false);
+        edtSearch.setHint("");
+        ivCancel.setImageResource(cancelDrawable);
+        if(null!=mOnStatusChangeListener){
+            mOnStatusChangeListener.onShowStartListener();
+        }
+    }
+
+    private void setShowSearchEnd() {
+        edtSearch.setCursorVisible(true);
+        edtSearch.requestFocus();
+        edtSearch.setHint(hint);
+        if(openWtihShowSoftInput){
+            imm.showSoftInput(edtSearch,InputMethodManager.SHOW_FORCED);
+        }
+        if(null!=mOnStatusChangeListener){
+            mOnStatusChangeListener.onShowEndListener();
+        }
     }
 
     @Override
@@ -164,9 +243,48 @@ public class CSearchView extends RelativeLayout {
         ivCancel = (ImageView) findViewById(R.id.iv_cancel);
         ivSearch = (ImageView) findViewById(R.id.iv_search);
         edtSearch = (CEditText) findViewById(R.id.edt_search);
-
-
     }
 
 
+    public void setOnSearchViewManagerListener(OnSearchViewManagerListener onSearchViewManagerListener) {
+        mOnSearchViewManagerListener = onSearchViewManagerListener;
+    }
+
+    public void setOnStatusChangeListener(OnStatusChangeListener onStatusChangeListener) {
+        mOnStatusChangeListener = onStatusChangeListener;
+    }
+
+    public interface OnSearchViewManagerListener{
+        void onTextChangeListener(CharSequence s);
+    }
+
+    public interface OnStatusChangeListener{
+        void onShowStartListener();
+        void onShowEndListener();
+        void onHideStartListener();
+        void onHideEndListener();
+    }
+
+
+    public abstract class StatusChangeListenerAdapter implements OnStatusChangeListener{
+        @Override
+        public void onHideEndListener() {
+
+        }
+
+        @Override
+        public void onHideStartListener() {
+
+        }
+
+        @Override
+        public void onShowEndListener() {
+
+        }
+
+        @Override
+        public void onShowStartListener() {
+
+        }
+    }
 }
