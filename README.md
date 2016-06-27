964ddc3dca86679e24bb45e7771461d7530987b4
Qian Cheng <csnowstack@gmail.com>
2016-06-27 15:42:06 +0800

    分离到module

--- README.md
+++ README.md
@@ -4,3 +4,10 @@
 预览
 
 ![MSearchView](https://github.com/CSnowStack/CSearchView/blob/master/imgs/csearchview.gif)
+
+使用
+```java
+dependencies {
+   compile 'com.cq.csearchview:csearchview:1.0.1'
+}
+```
---
+++ csearchview/src/main/java/com/cq/csearchview/DensityUtil.java
@@ -0,0 +1,53 @@
+package com.cq.csearchview;
+
+import android.app.Activity;
+import android.content.Context;
+import android.util.DisplayMetrics;
+
+import java.lang.reflect.Field;
+
+/**
+ *
+ */
+public class DensityUtil {
+
+    public static int px2dip(Context context, float pxValue) {
+        final float scale = context.getResources().getDisplayMetrics().density;
+        return (int) (pxValue / scale + 0.5f);
+    }
+
+    public static int dip2px(Context context, float dipValue) {
+        final float scale = context.getResources().getDisplayMetrics().density;
+        return (int) (dipValue * scale + 0.5f);
+    }
+
+    public static int px2sp(Context context, float pxValue) {
+        final float fontScale = context.getResources().getDisplayMetrics().scaledDensity;
+        return (int) (pxValue / fontScale + 0.5f);
+    }
+
+    public static int sp2px(Context context, float spValue) {
+        final float fontScale = context.getResources().getDisplayMetrics().scaledDensity;
+        return (int) (spValue * fontScale + 0.5f);
+    }
+
+    public static int systemBarHeight(Context context) {
+        int sbar = 0;
+        try {
+            Class<?> c = Class.forName("com.android.internal.R$dimen");
+            Object obj = c.newInstance();
+            Field field = c.getField("status_bar_height");
+            int x = Integer.parseInt(field.get(obj).toString());
+            sbar = context.getResources().getDimensionPixelSize(x);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return sbar;
+    }
+
+    public static int[] getScreenWidth(Activity context) {
+        DisplayMetrics displayMetrics = new DisplayMetrics();
+        context.getWindowManager().getDefaultDisplay().getMetrics(displayMetrics);
+        return new int[]{displayMetrics.widthPixels, displayMetrics.heightPixels};
+    }
+}
